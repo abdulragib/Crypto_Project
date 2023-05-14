eb78dacfbaad6d73ac57f5265825412650ea8e9e
Abdul Ragib <abdulragib36@gmail.com>
2023-05-14 22:20:45 +0530

    Working on header

---
+++ src/components/Common/Header/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import "./style.css"
+import TemporaryDrawer from "./drawer";
+const Header = () => {
+    return (
+        <div className="navbar">
+            <h1 className="logo">CryptoTracker<span style={{color:"var(--blue)"}}>.</span></h1>
+            <div className="links">
+                <a href="#">
+                    <p className="link">Home</p>
+                </a>
+                <a href="#">
+                    <p className="link">Watchlist</p>
+                </a>
+                <a href="#">
+                    <p className="link">Compare</p>
+                </a>
+                <a href="#">
+                    <p className="link">Dashboard</p>
+                </a>
+            </div>
+            <div className="mobile-drawer">
+                 <TemporaryDrawer/>
+            </div>
+        </div>
+    );
+};
+
+export default Header;
