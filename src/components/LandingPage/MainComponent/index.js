02261e733b35e32fd1ba495a248325926d9886af
Abdul Ragib <abdulragib36@gmail.com>
2023-05-16 10:53:10 +0530

    LandingPage -> Framer Motion Animation Done

--- src/components/LandingPage/MainComponent/index.js
+++ src/components/LandingPage/MainComponent/index.js
@@ -3,23 +3,44 @@ import './style.css'
 import Button from "../../Common/Button";
 import iphone from "../../../assets/iphone.png"
 import gradient from "../../../assets/gradient.png"
+import {motion} from 'framer-motion'
+
 const MainComponent = () => {
     return (
         <div className="flex-info">
-             <div className="left-component">
-                 <h1 className="track-crypto-heading">Track Crypto</h1>
-                 <h1 className="real-time-heading">Real Time.</h1>
-                 <p className="info-text">Track crypto through a public api in real time. Visit the dashboard to do so!</p>
+            <div className="left-component">
+                <motion.h1 initial={{opacity: 0, y: 50}}
+                           animate={{opacity: 1, y: 0}}
+                           transition={{duration: 0.5}}
+                           className="track-crypto-heading">Track Crypto
+                </motion.h1>
+                <motion.h1 initial={{opacity: 0, y: 50}}
+                           animate={{opacity: 1, y: 0}}
+                           transition={{duration: 0.5, delay: 0.5}} className="real-time-heading">Real Time.
+                </motion.h1>
+                <motion.p initial={{opacity: 0, y: 50}}
+                          animate={{opacity: 1, y: 0}}
+                          transition={{duration: 0.5, delay: 1}} className="info-text">Track crypto through a public api
+                    in real time. Visit the dashboard to do so!
+                </motion.p>
 
-                 <div className="btn-flex">
-                        <Button text={"Dashboard"}/>
-                        <Button text={"Share"} outlined={true}/>
-                 </div>
-             </div>
+                <motion.div
+                    initial={{opacity: 0, x: 50}}
+                    animate={{opacity: 1, x: 0}}
+                    transition={{duration: 0.5, delay: 1.5}}
+                    className="btn-flex">
+                    <Button text={"Dashboard"}/>
+                    <Button text={"Share"} outlined={true}/>
+                </motion.div>
+            </div>
 
             <div className="phone-container">
-                <img src={iphone} className='iphone'/>
-                <img src={gradient} className='gradient'/>
+                <motion.img
+                    initial={{y: -10}}
+                    animate={{y: 10}}
+                    transition={{type: "smooth", repeatType: "mirror", duration: 2, repeat: Infinity}}
+                    src={iphone} className='iphone' alt=""/>
+                <img src={gradient} className='gradient' alt=""/>
             </div>
         </div>
     );
