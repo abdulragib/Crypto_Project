4ed12dab2e50695c73a488bf71a71d816348acbe
Abdul Ragib <abdulragib36@gmail.com>
2023-05-20 14:59:09 +0530

    Coin Page -> Rendering List

--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -1,7 +1,48 @@
+import {useParams} from 'react-router-dom'
+import {useEffect, useState} from "react";
+import axios from "axios";
+import Header from "../components/Common/Header";
+import Loader from "../components/Common/Loader";
+import {coinObject} from "../functions/coinObject";
+import List from "../components/Dashboard/List";
+
 const Coin = () => {
-  <div>
-      Coin
-  </div>
+    const {id} = useParams();
+    const [coinData, setCoinData] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    useEffect(() => {
+        if (id) {
+            axios.get(`https://api.coingecko.com/api/v3/coins/${id}`)
+                .then((res) => {
+                    console.log("Response->", res);
+                    coinObject(setCoinData, res.data)
+                    setIsLoading(false);
+                })
+                .catch((err) => {
+                    console.log("Error>>", err);
+                    setIsLoading(false);
+                })
+        }
+    }, [id]);
+    return (
+        <div>
+            <Header/>
+            {isLoading ? <Loader/> :
+                <>
+                    <div>
+                        <tbody className="list-body" style={style}>
+                           <div className="grey-wrapper">
+                              <List coin={coinData}/>
+                           </div>
+                        </tbody>
+                    </div>
+                </>}
+        </div>
+    )
+}
+
+const style={
+    display:"block",
 }
 
 export default Coin
