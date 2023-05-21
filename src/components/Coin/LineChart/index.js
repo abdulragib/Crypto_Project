d654bfb7c5e7fccc997db1488476e999db7c8368
Abdul Ragib <abdulragib36@gmail.com>
2023-05-21 15:22:00 +0530

    LineChart -> Chart Added

--- src/components/Coin/LineChart/index.js
+++ src/components/Coin/LineChart/index.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import {Line} from "react-chartjs-2";
+import {CategoryScale, Chart as ChartJs, Legend, LinearScale, LineElement, Title, Tooltip,PointElement} from 'chart.js'; //Don't get rid of this
+ChartJs.register(
+    Title, Tooltip, LineElement, Legend, CategoryScale, LinearScale,PointElement
+)
 
-const LineChart = () => {
-    return (
-        <div>
+const LineChart = ({chartData, priceType, multiAxis}) => {
+    const options = {
+        plugins: {
+            legend: {
+                display: multiAxis ? true : false,
+            },
+        },
 
-        </div>
-    );
+        responsive: true,
+        interaction: {
+            mode: "index",
+            intersect: false,
+        }
+    };
+    return <Line data={chartData} options={options}/>
 };
+
 
 export default LineChart;
--- src/pages/Coin.js
+++ src/pages/Coin.js
@@ -1,6 +1,5 @@
 import {useParams} from 'react-router-dom'
 import {useEffect, useState} from "react";
-import axios from "axios";
 import Header from "../components/Common/Header";
 import Loader from "../components/Common/Loader";
 import {coinObject} from "../functions/coinObject";
@@ -8,32 +7,45 @@ import List from "../components/Dashboard/List";
 import CoinInfo from "../components/Coin/CoinInfo";
 import getCoinData from "../functions/getCoinData";
 import getCoinPrices from "../functions/getCoinPrices";
+import LineChart from "../components/Coin/LineChart";
 
 const Coin = () => {
     const {id} = useParams();
     const [coinData, setCoinData] = useState({});
     const [isLoading, setIsLoading] = useState(true);
     const [days, setDays] = useState(30);
+    const [chartData, setChartData] = useState({});
     useEffect(() => {
         if (id) {
-           getData()
+            getData()
         }
     }, [id]);
 
-    async function getData(){
+    async function getData() {
         const data = await getCoinData(id);
 
-        if(data){
-           coinObject(setCoinData,data);
-           const prices = await getCoinPrices(id,days);
+        if (data) {
+            coinObject(setCoinData, data);
+            const prices = await getCoinPrices(id, days);
 
-           if(prices.length>0)
-           {
-               console.log("Got Prices from API");
-               setIsLoading(false);
-           }
+            if (prices.length > 0) {
+                console.log("Got Prices from API");
+
+                setChartData({
+                    labels: ["Mon","Tue","Wed","Thur","Fri"],
+                    datasets: [{
+                        label:"First Data Set",
+                        data: [65, 59, 80, 81, 56],
+                        backgroundColor: "transparent",
+                        borderColor: "#3a80e9",
+                    }]
+                });
+
+                setIsLoading(false);
+            }
         }
     }
+
     return (
         <div>
             <Header/>
@@ -41,6 +53,9 @@ const Coin = () => {
                 <>
                     <div className="grey-wrapper">
                         <List coin={coinData}/>
+                    </div>
+                    <div className="grey-wrapper">
+                        <LineChart chartData={chartData}/>
                     </div>
                     <CoinInfo heading={coinData.name} desc={coinData.desc}/>
                 </>
